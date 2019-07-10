a0717314760dbd2d046e28b4a21ebcd12ab320ea
Pete Bacon Darwin <pete@bacondarwin.com>
2019-07-10 09:46:38 +0100

    implement lazy routes and ivy compilation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'customers',
+    loadChildren: async () => (await import('./customers/customers.module')).CustomersModule
+  },
+  {
+    path: 'orders',
+    loadChildren: async () => (await import('./orders/orders.module')).OrdersModule
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
